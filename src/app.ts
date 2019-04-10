9b104c8f90ab7169c8c0530b3cf026765032ae09
SIBHI S <sibhi.s@fayausa.com>
2019-04-10 17:21:47 +0530

    add store subscriptions functionality

--- src/app.ts
+++ src/app.ts
@@ -21,16 +21,27 @@ button.addEventListener(
       type: 'ADD_TODO',
       payload: payload
     })
-    console.log(store.value);
+    //console.log(store.value);
     input.value = '';
   },
   false
 );
 
+const unsubscribe = store.subscribe(state => {
+  renderTodos(state.todos.data)
+})
+destroy.addEventListener('click',unsubscribe,false)
+// destroy.addEventListener('click',
+//   ()=> {
+//   console.log('tets');
+//   },
+//   false
+// );
 todoList.addEventListener('click', function(event) {
   const target = event.target as HTMLButtonElement;
   if (target.nodeName.toLowerCase() === 'button') {
     console.log(target);
   }
 });
+
 store.subscribe(state => console.log('STATE::',state))
--- src/store/store.ts
+++ src/store/store.ts
@@ -5,7 +5,7 @@ export class Store {
   constructor(reducers={},initialState={}) {
     this.subscribers = [];
     this.reducers = reducers;
-    this.state = initialState;
+    this.state = this.reduce(initialState, {});
   }
   get value() {
     return this.state;
@@ -13,6 +13,9 @@ export class Store {
   subscribe(fn) {
     this.subscribers = [...this.subscribers,fn];
     this.notify();
+    return () => {
+      this.subscribers = this.subscribers.filter(sub => sub !== fn) //doubt
+    }
   }
   dispatch(action) {
     this.state = this.reduce(this.state,action);
@@ -26,7 +29,7 @@ export class Store {
     for(const prop in this.reducers){
       newState[prop] = this.reducers[prop](state[prop],action);
     }
-    console.log(newState);
+    //console.log(newState);
     return newState;
   }
 }
